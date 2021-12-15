44e131ec62e3b41fc96ac5f963842bb9c3ebfcea
Ira <tsaritsamira1@gmail.com>
2021-12-15 23:24:56 +0200

    Добавьте файлы проекта.

---
+++ SecretaryPanel.Designer.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+
+namespace clinic
+{
+    partial class SecretaryPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public EventHandler SecretaryPanel_Load { get; private set; }
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // SecretaryPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "SecretaryPanel";
+            this.Text = "SecretaryPanel";
+            this.Load += new System.EventHandler(this.SecretaryPanel_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
